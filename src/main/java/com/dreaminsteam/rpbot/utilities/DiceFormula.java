bb06af9a00761f552a8b25ef9b6dedaef1f84fa9
Jessyca <profnagorski@gmail.com>
2017-05-04 16:29:14 -0400

    Ok, fixed some stuff. I think I added all the years right? Still working on the mods, having a hard time figuring out how the roll them bones thing fits in to all the rest...

--- src/main/java/com/dreaminsteam/rpbot/utilities/DiceFormula.java
+++ src/main/java/com/dreaminsteam/rpbot/utilities/DiceFormula.java
@@ -5,32 +5,43 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class DiceFormula {
 
-	public static DiceFormula firstYearFormula = new DiceFormula(DiceType.D4, 1); //I'm creating a new formula for first years, and the formula has a two boxes, and the DiceType box had a d4, and the int box has a 1  
+	public static DiceFormula firstYearFormula = new DiceFormula(DiceType.D4, 0); //defines the different classes and what properties they have
+	public static DiceFormula secondYearFormula = new DiceFormula(DiceType.D6, 1);
+	public static DiceFormula thirdYearFormula = new DiceFormula(DiceType.D8, 2);
+	public static DiceFormula forthYearFormula = new DiceFormula(DiceType.D10, 3);
+	public static DiceFormula fifthYearFormula = new DiceFormula(DiceType.D12, 4);
+	public static DiceFormula sixYearFormula = new DiceFormula(DiceType.D14, 5);
+	public static DiceFormula seventhYearFormula = new DiceFormula(DiceType.D16, 6);
+	public static DiceFormula GraduateAFormula = new DiceFormula(DiceType.D18, 7);
+	public static DiceFormula GraduateBFormula = new DiceFormula(DiceType.D18, 8);
+	public static DiceFormula GraduateCFormula = new DiceFormula(DiceType.D18, 9);
+	public static DiceFormula MasteryFormula = new DiceFormula(DiceType.D20, 10);
 	
-	private DiceType die; 
+	private DiceType die; //names the properties
 	private int defaultModifier; 
 	
-	public DiceFormula(DiceType die, int defaultModifier){
+	public DiceFormula(DiceType die, int defaultModifier){ //shows how the properties are listed
 		this.die = die;
 		this.defaultModifier = defaultModifier;
 	} 
 	
 	public int rollDiceDefault(){
-		String formula = "1d" + die.getDieValue();
-		List<Integer> rollThemBones = DiceRoller.rollThemBones(formula);
+		String formula = "1d" + die.getDieValue(); //go to that list below and pull the value that's in the ()
+		List<Integer> rollThemBones = DiceRoller.rollThemBones(formula); 
 		int diceRolls = addList(rollThemBones);
-		return diceRolls + defaultModifier;
+		return diceRolls + defaultModifier; //I think this is bad??
 	}
 	
 	public int rollDiceWithModifiers(boolean withAdvantage, boolean withBurden, boolean inCombat){
 		if(withAdvantage){
-			int currentDie = die.ordinal(); //Ok, this finds the number of the current die in the list, yes?
-			int newDie = Math.max(currentDie+1, 8); //And this defines what the new die should be, which is the current die+1, no greater than the 8th option, yes?
-			DiceType dice = DiceType.values()[newDie]; //And this assigns a name "dice" to the new die, yeah?
+			int currentDie = die.ordinal(); //finds the number of the current die in the list
+			int newDie = Math.max(currentDie+1, 8); //defines the new die, which is the current die+1, no greater than the 8th option
+			DiceType dice = DiceType.values()[newDie]; //this assigns a name "dice" to the new die
 			
-			String formula = "1d" + dice.getDieValue();
+			String formula = "1d" + dice.getDieValue(); //go to that list below and pull the value that's in the ()
 			List<Integer> rollThemBones = DiceRoller.rollThemBones(formula);
-			return addList(rollThemBones);
+			int diceRolls = addList(rollThemBones);
+			return diceRolls + defaultModifier;
 			
 		}
 		if(withBurden){
@@ -53,7 +64,7 @@ public class DiceFormula {
 		return result.get();
 	}
 	
-	public static enum DiceType{ //ok, so every time the above code looks for DiceType, it looks here? And each one has a name (D4) a value (4) and a number in the list (0)?
+	public static enum DiceType{ //list of DiceType. defined as name (D4) value (4) and placement in the list (0)
 		D4(4), //This is 0
 		D6(6), //This is 1
 		D8(8), //This is 2
